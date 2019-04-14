62d83b8626d9d298122b07c4f595d0c5ceeb7918
Denis Gilevskiy <fatalgryzin@gmail.com>
2019-04-14 18:53:20 +0300

    Add FileValidator

---
+++ src/main/java/com/epam/finaltask/validation/FileValidator.java
@@ -0,0 +1,27 @@
+package com.epam.finaltask.validation;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+
+public class FileValidator {
+
+    private static final Logger logger = LogManager.getLogger();
+
+    public boolean validate(String filename) {
+
+        if (filename == null) {
+            logger.log(Level.WARN, "filename argument is null, validation result is false");
+            return false;
+        }
+
+        logger.log(Level.DEBUG, "argument " + filename);
+        File file = new File(filename);
+
+        boolean result = file.exists() && file.isFile() && file.canRead() && (file.length() != 0);
+        logger.log(Level.INFO, "file validation result is " + result);
+        return result;
+    }
+}
