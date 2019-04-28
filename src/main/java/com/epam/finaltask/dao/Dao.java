d309bb1a76197f4f52552d818cfe6cdea35b05d8
Denis Gilevskiy <fatalgryzin@gmail.com>
2019-04-29 00:11:14 +0300

    Add Dao interface

--- src/main/java/com/epam/finaltask/dao/Dao.java
+++ src/main/java/com/epam/finaltask/dao/Dao.java
@@ -1,13 +1,19 @@
 package com.epam.finaltask.dao;
 
+import com.epam.finaltask.dao.impl.PersistenceException;
 import com.epam.finaltask.entity.Entity;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
 public interface Dao<T extends Entity> {
-    List<T> findAll();
-    T findEntityById(long id);
-    boolean create(T entity);
-    boolean delete(long id);
-    boolean update(T entity);
+    Logger logger = LogManager.getLogger();
+
+    List<T> findAll() throws PersistenceException;
+    int update(T entity) throws PersistenceException;
+    boolean create(T entity) throws PersistenceException;
+    T findEntityById(long id) throws PersistenceException;
+    String getDeleteByIdQuery();
+    boolean delete(long id) throws PersistenceException;
 }
