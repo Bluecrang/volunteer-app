ab8dced53f4cbc9a374298fdd3a3c49e4d024e50
Denis Gilevskiy <fatalgryzin@gmail.com>
2019-04-29 00:08:24 +0300

    Add SaltGenerator

---
+++ src/main/java/com/epam/finaltask/util/SaltGenerator.java
@@ -0,0 +1,15 @@
+package com.epam.finaltask.util;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+public class SaltGenerator {
+
+    private static final int HEX_RADIX = 16;
+    private static final int NUMBER_OF_BITS = 128;
+
+    public String generateSalt() {
+        SecureRandom random = new SecureRandom();
+        return new BigInteger(NUMBER_OF_BITS, random).toString(HEX_RADIX);
+    }
+}
