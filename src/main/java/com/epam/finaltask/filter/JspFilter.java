358212aff5dca3b5383d2e1e78e3cc012df59b93
Denis Gilevskiy <fatalgryzin@gmail.com>
2019-05-26 00:34:42 +0300

    Add JspFilter

---
+++ src/main/java/com/epam/finaltask/filter/JspFilter.java
@@ -0,0 +1,40 @@
+package com.epam.finaltask.filter;
+
+import com.epam.finaltask.util.ApplicationConstants;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Filter which forbids access to jsp files.
+ */
+@WebFilter(dispatcherTypes = {DispatcherType.REQUEST}, filterName = "JspFilter")
+public class JspFilter implements Filter{
+
+    private static final Logger logger = LogManager.getLogger();
+
+    private static final Pattern JSP_EXTENSION_PATTERN = Pattern.compile(".*[.]jsp$");
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        String uri = httpRequest.getRequestURI();
+        logger.log(Level.DEBUG, "uri = " + uri);
+        Matcher matcher = JSP_EXTENSION_PATTERN.matcher(uri);
+        if (matcher.matches()) {
+            logger.log(Level.DEBUG, "jsp extension found, sending error");
+            httpResponse.sendError(ApplicationConstants.PAGE_NOT_FOUND_ERROR_CODE);
+            return;
+        }
+        chain.doFilter(request, response);
+    }
+}
